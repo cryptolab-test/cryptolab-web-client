e774f1224953539fe95eedbb543909bf764f172d
andhl <andhlwhffu@gmail.com>
2022-12-06 17:36:39 +0900

    init: initial next app

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <div className={styles.container}></div>
+    </>
+  );
+}
